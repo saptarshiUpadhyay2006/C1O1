5e7ead069f5006e32274e651a1f40edb214f76c6
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-05 22:55:21 +0530

    Kth pascal--Optimal

--- Google/Easy/p1.java
+++ Google/Easy/p1.java
@@ -1,5 +1,23 @@
+//kth row of pascal triangle(LC 119)
+//Optimal----Observe the pattern-----T.C:O(n)------S.C:O(1)
+//USING THE LC TEMPLATE
 package Google.Easy;
+import java.util.*;
 
-public class p1 {
-    
+class p1{
+    public List<Integer> getRow(int rowIndex) {
+        int n=rowIndex;
+        int arr[]=new int[n+1];
+        arr[0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=i;j>0;j--)
+            arr[j]+=arr[j-1];
+        }
+        List<Integer> l=new ArrayList<>();
+        for(int i=0;i<arr.length;i++)
+        l.add(arr[i]);
+        return l;
+    }
+
 }
---
+++ Google/Easy/p2.java
@@ -0,0 +1,4 @@
+//Invert BT(LC 226)
+//Optimal----Observe the pattern-----T.C:O(n)------S.C:O(1)
+//USING THE LC TEMPLATE
+package Google.Easy;
