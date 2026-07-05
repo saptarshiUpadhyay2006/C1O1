0792c02001e3ce29225905c8df31004ba318f4b4
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-05 21:59:15 +0530

    Min path sum--optimal

--- GS/Medium/p4.java
+++ GS/Medium/p4.java
@@ -1,8 +1,27 @@
 //Minm path sum(LC 64)
-//-----T.C:O(n)------S.C:O(n)
+//Optimal----DP-----T.C:O(n^2)------S.C:O(n)
 //USING THE LC TEMPLATE
 package GS.Medium;
 
-public class p4{
-    
+class p4{
+    public int minPathSum(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int dp[][]=new int[m][n];
+        dp[0][0]=grid[0][0];
+        for(int i=1;i<m;i++)
+        dp[i][0]=dp[i-1][0]+grid[i][0];
+        
+        for(int j=1;j<n;j++)
+        dp[0][j]=dp[0][j-1]+grid[0][j];
+
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
 }
