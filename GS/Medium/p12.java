1e24a8371eacfcda088df105a7042d5bd4e160c1
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-08 21:31:14 +0530

    Fraction to decimal---optimal

--- GS/Medium/p12.java
+++ GS/Medium/p12.java
@@ -1,4 +1,40 @@
 //Fraction to recurring Decimal(LC 166)
-//-----T.C:O(n)------S.C:O(n)
+//Optimal-----HashMap & while condn-----T.C:O(d)------S.C:O(d)
 //USING THE LC TEMPLATE
 package GS.Medium;
+import java.util.*;
+
+class p12{
+    public String fractionToDecimal(int numerator, int denominator) {
+        if(numerator==0)
+        return "0";
+        StringBuilder sb=new StringBuilder();
+        sb.append(((numerator>0)^(denominator>0))?"-":"");
+
+        long n=Math.abs((long)numerator);
+        long d=Math.abs((long)denominator);
+        sb.append(n/d);
+        n%=d;
+        if(n==0)
+        return sb.toString();
+        sb.append(".");
+        HashMap<Long,Integer> mp=new HashMap<Long,Integer>();
+        mp.put(n,sb.length());
+        while(n!=0)
+        {
+            n*=10;
+            sb.append(n/d);
+            n%=d;
+            if(mp.containsKey(n))
+            {
+                int idx=mp.get(n);
+                sb.insert(idx,"(");
+                sb.append(")");
+                break;
+            }
+            else
+            mp.put(n,sb.length());
+        }
+        return sb.toString();
+    }
+}
