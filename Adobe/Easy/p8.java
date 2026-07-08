60f37347384d28191fd89803e3fc4a678135c8d1
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-08 15:43:10 +0530

    length of last word--optimal

--- Adobe/Easy/p8.java
+++ Adobe/Easy/p8.java
@@ -1,6 +1,23 @@
 //Length of last word(LC 58)
-//Optimal Approach---Basic ops with manipulation----T.C:O(n)------S.C:O(n)
+//Optimal Approach---Traversal from last----T.C:O(n)------S.C:O(1)
 //USING THE LC TEMPLATE
 
 package Adobe.Easy;
 
+class p8{
+    public int lengthOfLastWord(String s) {
+        int c=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char ch=s.charAt(i);
+            if(ch!=' ')
+            c++;
+            else 
+            {
+                if(c>0)
+                return c;
+            }
+        }
+        return 0;
+    }
+}
