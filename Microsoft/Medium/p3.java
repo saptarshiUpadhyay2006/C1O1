41f794ebca6eb938a4b607aeda5f9ab06b273414
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-11 16:35:21 +0530

    LL cycle---optimal

--- Microsoft/Medium/p3.java
+++ Microsoft/Medium/p3.java
@@ -1,5 +1,26 @@
 //Linked List Cycle(LC 141)
-//Optimal------T.C:O(n)------S.C:O(n)
+//Optimal----Use slow & fast pointer-------T.C:O(n)------S.C:O(n)
 //USING THE LC TEMPLATE
 
 package Microsoft.Medium;
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+    }
+}
+public class p3{
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+            return true;
+        }
+        return false;
+    }
+}
