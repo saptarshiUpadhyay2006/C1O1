d079174db4d711a283ebd7380b5cfe885a8cc364
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-12 14:53:57 +0530

    LIS--optimal

--- Microsoft/Hard/p1.java
+++ Microsoft/Hard/p1.java
@@ -1,5 +1,27 @@
-package Microsoft.Hard;
+//Longest Increasing Subsequence(LC 300)
+//Optimal---Use Dp----T.C:O(nlogk)------S.C:O(k)
+//USING THE LC TEMPLATE
 
-public class p1 {
-    
+package Microsoft.Hard;
+import java.util.*;
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums[j]<nums[i])
+                dp[i]=Math.max(dp[i],dp[j]+1);
+            }
+        }
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            res=Math.max(res,dp[i]);
+        }
+        return res;
+    }
 }
