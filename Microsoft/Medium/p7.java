4a82d4b9698211ace699c08bc22578c3ec96c42f
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-12 11:19:08 +0530

    longest substring,del node in LL---optimal

--- Microsoft/Medium/p7.java
+++ Microsoft/Medium/p7.java
@@ -3,3 +3,21 @@
 //USING THE LC TEMPLATE
 
 package Microsoft.Medium;
+class p7{
+    public void deleteNode(ListNode node) {
+        ListNode prev=null;
+        while(node.next!=null)
+        {
+            node.val=node.next.val;
+            prev=node;
+            node=node.next;
+        }
+        prev.next=null;
+    }
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
--- Microsoft/Medium/p8.java
+++ Microsoft/Medium/p8.java
@@ -1,5 +1,30 @@
-//Longest Substring(LC )
+//Longest Substring without repeating characters(LC 3)
 //Optimal------T.C:O(n)------S.C:O(n)
 //USING THE LC TEMPLATE
 
 package Microsoft.Medium;
+import java.util.*;
+
+class p8{
+    public int lengthOfLongestSubstring(String s) {
+        int fq[]=new int[256];
+        Arrays.fill(fq,0);
+        int n=s.length(),i=0,j=0,sm=0;
+
+        while(i<n)
+        {
+            if(fq[s.charAt(i)]==1)
+            {
+                while(fq[s.charAt(i)]==1)
+                {
+                    fq[s.charAt(j)]--;
+                    j++;
+                }
+            }
+            fq[s.charAt(i)]++;
+            sm=Math.max(sm,i-j+1);
+            i++;
+        }
+        return sm;
+    }
+}
