d7ad004518ba0eab1ef2897c460d8ae8aa202257
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-13 15:57:50 +0530

    Design stack with increment op---optimal

--- Atlassian/Medium/p2.java
+++ Atlassian/Medium/p2.java
@@ -1,6 +1,47 @@
 //Design a Stack With Increment Operation(LC 1381)
-//Optimal---------T.C:O(n^2)------S.C:O(n^2)
+//Optimal----Basics op of stack-----T.C:O(1)--Increment:O(n)----S.C:O(1)--Increment:O(n)
 //USING THE LC TEMPLATE
 
 package Atlassian.Medium;
 import java.util.*;
+
+class p2{
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+    int cs,ms;
+    public p2(int maxSize) {
+        cs=0;
+        ms=maxSize;
+        s1=new Stack<>();
+        s2=new Stack<>();
+    }
+    
+    public void push(int x) {
+        if(cs<ms)
+        {
+            s1.push(x);
+            cs++;
+        }
+    }
+    
+    public int pop() {
+        if(cs==0)
+        return -1;
+
+        cs--;
+        return s1.pop();
+    }
+    
+    public void increment(int k, int val) {
+        while(s1.size()!=0)
+        s2.push(s1.pop());
+
+        while((s2.size()!=0) && (k>0))
+        {
+            s1.push(s2.pop()+val);
+            k--;
+        }
+        while(s2.size()!=0)
+        s1.push(s2.pop());
+    }
+}
