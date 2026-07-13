b5c48f828724a0cd93d8626552f0311d971d4cda
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-13 17:13:19 +0530

    top k freq words---optimal

--- Atlassian/Medium/p7.java
+++ Atlassian/Medium/p7.java
@@ -1,6 +1,35 @@
 //Rank Team by votes(LC 1366)
-//Optimal---------T.C:O(n^2)------S.C:O(n^2)
+//Optimal---------T.C:O(n^2logn)------S.C:O(n^2)
 //USING THE LC TEMPLATE
 
 package Atlassian.Medium;
 import java.util.*;
+
+class p7{
+    public String rankTeams(String[] votes) {
+        Map<Character,int[]> mp=new HashMap<>();
+        int l=votes[0].length();
+        for(String v:votes)
+        {
+            for(int j=0;j<l;j++)
+            {
+                char ch=v.charAt(j);
+                mp.putIfAbsent(ch,new int[l]);
+                mp.get(ch)[j]++;
+            }
+        }
+        List<Character> arr=new ArrayList<>(mp.keySet());
+        Collections.sort(arr,(a,b)->{
+            for(int j=0;j<l;j++)
+            {
+                if(mp.get(a)[j]!=mp.get(b)[j])
+                return mp.get(b)[j]-mp.get(a)[j];
+            }
+            return a-b;
+        });
+        StringBuilder sb=new StringBuilder();
+        for(char ch:arr)
+        sb.append(ch);
+        return sb.toString();
+    }
+}
--- Atlassian/Medium/p8.java
+++ Atlassian/Medium/p8.java
@@ -1,6 +1,30 @@
 //Top k Freq words(LC 692)
-//Optimal---------T.C:O(n^2)------S.C:O(n^2)
+//Optimal---------T.C:O(n+mlogm+klogm)------S.C:O(m)
 //USING THE LC TEMPLATE
 
 package Atlassian.Medium;
 import java.util.*;
+
+class p8{
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> mp=new HashMap<>();
+        for(String w:words) {
+            mp.put(w,mp.getOrDefault(w,0)+1);
+        }
+        PriorityQueue<String> pq=new PriorityQueue<>(
+            (a, b)->{
+                int fa=mp.get(a);
+                int fb=mp.get(b);
+                if(fa==fb)
+                    return a.compareTo(b);
+                return fb-fa; 
+            }
+        );
+        pq.addAll(mp.keySet());
+        List<String> arr=new ArrayList<>();
+        for(int i=0;i<k;i++){
+            arr.add(pq.poll());
+        }
+        return arr;
+    }
+}
