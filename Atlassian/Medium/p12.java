051826de95d40db22fb96244719d4a411459e83e
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-13 19:00:12 +0530

    smallest missing non-negative after ops---optimal

--- Atlassian/Medium/p12.java
+++ Atlassian/Medium/p12.java
@@ -1,7 +1,32 @@
 //Unique paths II(LC 63)
-//Optimal---------T.C:O(n^2)------S.C:O(n^2)
+//Optimal---------T.C:O(n.m)------S.C:O(n.m)
 //USING THE LC TEMPLATE
 
 package Atlassian.Medium;
 import java.util.*;
 
+class p12{
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n=obstacleGrid.length;
+        int m=obstacleGrid[0].length;
+        int dp[][]=new int[n][m];
+        dp[n-1][m-1]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                if(obstacleGrid[i][j]==1)
+                {
+                    dp[i][j]=0;
+                    continue;
+                }
+                if(i+1<n)
+                dp[i][j]=dp[i+1][j];
+
+                if(j+1<m)
+                dp[i][j]+=dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
--- Atlassian/Medium/p13.java
+++ Atlassian/Medium/p13.java
@@ -1,8 +1,38 @@
 //Smallest Missing Non-negative Integer After Operations(LC 2598)
-//Optimal---------T.C:O(n^2)------S.C:O(n^2)
+//Optimal---------T.C:O(n)------S.C:O(n)
 //USING THE LC TEMPLATE
 
 package Atlassian.Medium;
 import java.util.*;
 
-
+class p13{
+    public int findSmallestInteger(int[] nums, int value) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            int v=(nums[i]%value+value)%value;
+            if(v<n)
+            arr[v]++;
+        }
+        int res=n;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+            {
+                res=i;
+                break;
+            }
+            else
+            {
+                arr[i]--;
+                if(arr[i]>=1 && i+value<n)
+                {
+                    arr[i+value]+=arr[i];
+                    arr[i]=0;
+                }
+            }
+        }
+        return res;
+    }
+}
