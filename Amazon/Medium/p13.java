95deb24d235d8f7ecca412f5d5f333a926064cc7
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-14 12:02:39 +0530

    jump game---optimal

--- Amazon/Medium/p13.java
+++ Amazon/Medium/p13.java
@@ -1,5 +1,32 @@
 //Jump game(LC 55)
-//Optimal Approach----T.C:O(n)------S.C:O(1)
+//Optimal Approach----T.C:O(n^2)------S.C:O(n)
 //USING THE LC TEMPLATE
 package Amazon.Medium;
+import java.util.*;
+class p13{
+    HashMap<Integer,Boolean> mp;
+    public boolean canJump(int[] nums) {
+        mp=new HashMap<>();
+        return fac(nums,0);
+    }
+    private boolean fac(int nums[],int i)
+    {
+        if(i==nums.length-1)
+        return true;
+        if(i>=nums.length||nums[i]==0)
+        return false;
 
+        if(mp.containsKey(i))
+        return false;
+        boolean b=false;
+        for(int j=1;j<=nums[i];j++)
+        {
+            b=fac(nums,i+j);
+            if(b)
+            return true;
+        }
+        mp.put(i,false);
+
+        return false;
+    }
+}
