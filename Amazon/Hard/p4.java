84908beafbe6952018e50177d458d9d9e0a75936
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-15 20:25:47 +0530

    median from data stream---optimal

--- Amazon/Hard/p4.java
+++ Amazon/Hard/p4.java
@@ -3,3 +3,31 @@
 //USING THE LC TEMPLATE
 
 package Amazon.Hard;
+import java.util.*;
+class MedianFinder {
+    PriorityQueue<Integer> mini=new PriorityQueue<>();
+    PriorityQueue<Integer> maxi=new PriorityQueue<>(Collections.reverseOrder());
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        maxi.offer(num);
+        mini.offer(maxi.poll());
+        if(mini.size()>maxi.size())
+        maxi.offer(mini.poll());
+    }
+    
+    public double findMedian() {
+        if(maxi.size()>mini.size())
+        return maxi.peek();
+        return (mini.peek()+maxi.peek())/2.0d;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
