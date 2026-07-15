f3c7a56b3d56a00349c4482aa07236553d7d456b
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-15 20:55:31 +0530

    racecar & longest valid parentheses---optimal

--- Amazon/Hard/p5.java
+++ Amazon/Hard/p5.java
@@ -1,5 +1,28 @@
 //Race car(LC 818)
-//Optimal Approach----T.C:O(m*n)------S.C:O(n)
+//Optimal Approach----T.C:O(TlogT)------S.C:O(TlogT)
 //USING THE LC TEMPLATE
-
 package Amazon.Hard;
+import java.util.*;
+
+class p5{
+    public int racecar(int target) {
+        int dp[]=new int[target+1];
+        int n=1;
+        for(int i=1;i<=target;i++)
+        {
+            if(i==(1<<n)-1)
+            {
+                dp[i]=n;
+                n++;
+            }
+            else{
+                dp[i]=Integer.MAX_VALUE;
+                for(int j=0;j<n-1;j++)
+                dp[i]=Math.min(dp[i],n+j+1+dp[i-((1 << (n - 1)) - (1 << j))]);
+                
+                dp[i]=Math.min(dp[i],n+1+dp[(1<<n)-1-i]);
+            }
+        }
+        return dp[target];
+    }
+}
--- Amazon/Hard/p6.java
+++ Amazon/Hard/p6.java
@@ -1,5 +1,29 @@
 //Longest valid parentheses(LC 32)
-//Optimal Approach----T.C:O(m*n)------S.C:O(n)
+//Optimal Approach----T.C:O(n)------S.C:O(n)
 //USING THE LC TEMPLATE
 
 package Amazon.Hard;
+import java.util.*;
+class p5{
+    public int longestValidParentheses(String s) {
+        int n=s.length();
+        Stack<Integer> st=new Stack<>();
+        st.push(-1);
+        int res=0;
+        for(int i=0;i<n;i++)
+        {
+            if(s.charAt(i)=='(')
+            st.push(i);
+            else
+            {
+                if(!st.empty())
+                st.pop();
+                if(!st.empty())
+                res=Math.max(res,i-st.peek());
+                else
+                st.push(i);
+            }
+        }
+        return res;
+    }
+}
