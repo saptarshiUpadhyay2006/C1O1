dccd6a5009df44981fbec90ada9470b911c11287
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-20 22:47:19 +0530

    GS2--optimal

---
+++ GoldmanSachs/Med/p2.java
@@ -0,0 +1,21 @@
+//Corporate flight bookings(LC 1109)
+//Use a difference array
+
+package GoldmanSachs.Med;
+
+class p2{
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int arr[]=new int[n];
+        for(int b[]:bookings)
+        {
+            arr[b[0]-1]+=b[2];
+            if(b[1]<n)
+            arr[b[1]]-=b[2];
+        }
+        for(int i=1;i<n;i++)
+        {
+            arr[i]+=arr[i-1];
+        }
+        return arr;
+    }
+}
