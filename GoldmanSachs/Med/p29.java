ff8d1af0757c42afa9424051f91a214908f48230
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-23 20:55:15 +0530

    GS-2

--- GoldmanSachs/Med/p29.java
+++ GoldmanSachs/Med/p29.java
@@ -1,2 +1,25 @@
+//Minm moves to capture the Queen(LC 3001)
+//check the optimal moves and compare accordingly
+
 package GoldmanSachs.Med;
 
+class p29{
+    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        if(a==e||b==f){
+            if(a==e&&a==c&&((d-b)*(d-f)<0)) 
+            return 2;
+            if(b==f&&b==d&&((c-a)*(c-e)<0)) 
+            return 2;
+            
+            return 1;
+        }
+        if(Math.abs(c-e)==Math.abs(d-f))
+        {
+            if(Math.abs(c-a)==Math.abs(d-b)&&Math.abs(e-a)==Math.abs(f-b)&&((b-f)*(b-d)<0)) 
+            return 2;
+            
+            return 1;
+        }
+        return 2;
+    }
+}
--- GoldmanSachs/Med/p30.java
+++ GoldmanSachs/Med/p30.java
@@ -1,2 +1,25 @@
+//Minm length of String after deleting similar ends(LC 1750)
+//Using a peeling technique
+
 package GoldmanSachs.Med;
 
+class Solution {
+    public int minimumLength(String s) {
+        int i=0,j=s.length()-1;
+        while(i<j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            break;
+
+            if(s.charAt(i)==s.charAt(j))
+            {
+                char ch=s.charAt(i);
+                while(i<=j && s.charAt(i)==ch)
+                i++;
+                while(j>=i && s.charAt(j)==ch)
+                j--;
+            }
+        }
+        return j-i+1;
+    }
+}
