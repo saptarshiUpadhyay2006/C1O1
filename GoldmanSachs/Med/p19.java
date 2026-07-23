90fdd8f0706ec7cb993fba550a0467eac5e930a2
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-07-23 11:41:45 +0530

    GS-3

--- GoldmanSachs/Med/p19.java
+++ GoldmanSachs/Med/p19.java
@@ -1,2 +1,17 @@
+//Best time to buy & sell stock(LC 121)
+//iteration keeping track of max profit
+
 package GoldmanSachs.Med;
 
+class p19{
+    public int maxProfit(int[] prices) {
+        int mp=0,mini=prices[0];
+        for(int i=0;i<prices.length;i++)
+        {
+            int c=prices[i]-mini;
+            mp=Math.max(c,mp);
+            mini=Math.min(mini,prices[i]);
+        }
+        return mp;
+    }
+}
--- GoldmanSachs/Med/p20.java
+++ GoldmanSachs/Med/p20.java
@@ -1,2 +1,21 @@
+//Find Minm in rotated sorted array(LC 153)
+//using binary search to find the minm using bitwise operator
+
 package GoldmanSachs.Med;
 
+class p20{
+    public int findMin(int[] nums) {
+        int n=nums.length-1;
+        int lst=nums[n];
+        int l=0,r=n;
+        while(l<r)
+        {
+            int m=(l+r)>>1;
+            if(nums[m]>lst)
+            l=m+1;
+            else
+            r=m;
+        }
+        return nums[l];
+    }
+}
