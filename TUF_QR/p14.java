d326e1d02448ff23e3cfc50443af0f1946352605
Saptarshi Upadhyay <upadhyay.saptarshi@gmail.com>
2026-08-05 13:05:43 +0530

    QR-2

--- TUF_QR/p14.java
+++ TUF_QR/p14.java
@@ -1,5 +1,23 @@
 package TUF_QR;
 
-public class p14 {
-    
+class p14{
+    public int findPlatform(int[] Arrival, int[] Departure) {
+        //your code goes here
+        int n=Arrival.length;
+        int res=1;
+        for(int i=0;i<n;i++)
+        {
+            int c=1;
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j)
+                {
+                    if((Arrival[i]>=Arrival[j] && Departure[j]>=Arrival[i]))
+                    c++;
+                    res=Math.max(res,c);
+                }
+            }
+        }
+        return res;
+    }
 }
--- TUF_QR/p15.java
+++ TUF_QR/p15.java
@@ -1,5 +1,33 @@
 package TUF_QR;
+import java.util.*;
+class p15{
+    static class MeetingComparator implements Comparator<int[]>{
+        public int compare(int a[],int b[])
+        {
+            return Integer.compare(a[1],b[1]);
+        }
+    }
+    public int maxMeetings(int[] start, int[] end) {
+       //your code goes here
+        int n=start.length;
+        List<int[]> arr=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            arr.add(new int[]{start[i],end[i]});
+        }
 
-public class p15 {
-    
+        Collections.sort(arr,new MeetingComparator());
+        int l=arr.get(0)[1];
+        int c=1;
+
+        for(int i=1;i<n;i++)
+        {
+            if(arr.get(i)[0]>l)
+            {
+                l=arr.get(i)[1];
+                c++;
+            }
+        }
+        return c;
+    }
 }
